51ed21e9729a6a7f78d5854e37050e9ad23fd8fc
tim <tparys@gmail.com>
2021-01-05 15:25:37 -0500

    Break out build process, make inner logic more flexible

--- runme.sh
+++ runme.sh
@@ -1,64 +1,27 @@
 #!/bin/bash
 set -e
 
-PACKAGES="qemu-user-static build-essential cmake"
-PACKAGES+=" libstdc++6:armhf g++-arm-linux-gnueabihf"
-PACKAGES+=" libstdc++6:arm64 g++-aarch64-linux-gnu"
+# Configuration (TBD - parse args)
+export DISTRO=ubuntu
+export CODENAME=focal
+export EXTRA_ARCH="i386 armhf arm64"
+BUILD_CONTEXT=build
 
-make_apt_sources()
-{
-    cat <<EOF
-# x86 Debians
-deb [arch=i386,amd64] http://us.archive.ubuntu.com/ubuntu/ ${CODENAME} main restricted universe
-deb [arch=i386,amd64] http://us.archive.ubuntu.com/ubuntu/ ${CODENAME}-updates main restricted universe
-deb [arch=i386,amd64] http://us.archive.ubuntu.com/ubuntu/ ${CODENAME}-security main restricted universe
+# Reset build context
+rm -rf ${BUILD_CONTEXT}
+cp -r template ${BUILD_CONTEXT}
 
-# ARM Debians
-deb [arch=armhf,arm64] http://ports.ubuntu.com/ ${CODENAME} main restricted universe
-deb [arch=armhf,arm64] http://ports.ubuntu.com/ ${CODENAME}-updates main restricted universe
-deb [arch=armhf,arm64] http://ports.ubuntu.com/ ${CODENAME}-security main restricted universe
-EOF
-}
+# Substitute variables
+pushd ${BUILD_CONTEXT}
+for file in $(find . -name '*.in' -type f -print); do
+    envsubst < ${file} > ${file//.in/}
+    rm ${file}
+done
+popd
 
-make_docker_file()
-{
-    cat <<EOF
-FROM ${DISTRO}:${CODENAME} as builder
-LABEL stage=builder
-
-# Multiarch apt
-RUN dpkg --add-architecture armhf
-RUN dpkg --add-architecture arm64
-COPY sources.list /etc/apt/
-RUN apt-get update
-
-# Development packages
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y ${PACKAGES}
-
-# Helpful stuff
-COPY hello.c /
-COPY hello.cpp /
-
-# Compile some stuff!
-RUN x86_64-linux-gnu-gcc -o hello_c_amd64 hello.c
-RUN x86_64-linux-gnu-g++ -o hello_cpp_amd64 hello.cpp
-RUN arm-linux-gnueabihf-gcc -o hello_c_armhf hello.c
-RUN arm-linux-gnueabihf-g++ -o hello_cpp_armhf hello.cpp
-RUN aarch64-linux-gnu-gcc -o hello_c_arm64 hello.c
-RUN aarch64-linux-gnu-g++ -o hello_cpp_arm64 hello.cpp
-
-# Stuff that doesn't need to be kept
-#RUN rm -fr /var/lib/apt/lists/*
-#RUN rm -fr /var/cache/apt/archives/*
-EOF
-}
-
-DISTRO=ubuntu
-CODENAME=focal
-TAG=omnibuild:${CODENAME}
+# Build/run the image
+TAG=obuild:${CODENAME}
 docker rmi -f ${TAG}
-make_apt_sources > sources.list
-make_docker_file ${DISTRO} ${CODENAME} > Dockerfile
-docker build -t ${TAG} .
+docker build -t ${TAG} ${BUILD_CONTEXT}
 docker image prune -f --filter label=stage=builder
 docker run -it ${TAG} /bin/bash
---
+++ template/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+set -x
+
+DISTRO="${1}"
+CODENAME="${2}"
+EXTRA_ARCH="${3}"
+
+for ARCH in ${EXTRA_ARCH}; do
+    dpkg --add-architecture ${ARCH}
+done
+
+# Pull software lists
+apt-get update
+
+# Basic packages
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y qemu-user-static build-essential cmake dpkg-dev devscripts
+
+# Cross Compilers, basic libraries for each arch
+for ARCH in ${EXTRA_ARCH}; do
+    GCC_TUPLE=$(dpkg-architecture -q DEB_TARGET_GNU_TYPE -A ${ARCH})
+    apt-get install -y libstdc++6:${ARCH} g++-${GCC_TUPLE}
+done
