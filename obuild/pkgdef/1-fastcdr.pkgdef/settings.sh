6ed27efb17fbaf90d1fb550f66a805e1d7ec3c41
tim <tparys@gmail.com>
2021-06-16 21:07:52 -0400

    Fast-DDS 2.3.3

--- obuild/pkgdef/1-fastcdr.pkgdef/settings.sh
+++ obuild/pkgdef/1-fastcdr.pkgdef/settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export DEB_PKG_NAME=fastcdr
-export DEB_VERSION=1.0.17
+export DEB_VERSION=1.0.21
 export GIT_REPO=https://github.com/eProsima/Fast-CDR.git
-export GIT_TAG=v1.0.17
+export GIT_TAG=v1.0.21
--- obuild/pkgdef/2-fastdds.pkgdef/settings.sh
+++ obuild/pkgdef/2-fastdds.pkgdef/settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export DEB_PKG_NAME=fastdds
-export DEB_VERSION=2.1.0
+export DEB_VERSION=2.3.3
 export GIT_REPO=https://github.com/eProsima/Fast-DDS.git
-export GIT_TAG=v2.1.0
+export GIT_TAG=v2.3.3
