40636f9239667bf930887800615e916a640a2b12
tim <tparys@gmail.com>
2021-07-26 16:58:39 -0400

    Rollback versions to Fast-DDS v2.0.1 for testing

--- obuild/pkgdef/2-fastdds.pkgdef/settings.sh
+++ obuild/pkgdef/2-fastdds.pkgdef/settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export DEB_PKG_NAME=fastdds
-export DEB_VERSION=2.3.3
+export DEB_VERSION=2.0.1
 export GIT_REPO=https://github.com/eProsima/Fast-DDS.git
-export GIT_TAG=v2.3.3
+export GIT_TAG=v2.0.1
--- obuild/pkgdef/4-fastddsgen.pkgdef/settings.sh
+++ obuild/pkgdef/4-fastddsgen.pkgdef/settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export DEB_PKG_NAME=fastddsgen
-export DEB_VERSION=2.0.1
+export DEB_VERSION=1.0.4
 export GIT_REPO=https://github.com/eProsima/Fast-DDS-Gen.git
-export GIT_TAG=v2.0.1
+export GIT_TAG=v1.0.4
